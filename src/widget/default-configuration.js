e302bdf6419272bc769cd8438ce24f86cd540e89
Mohd Danish <mddanishyusuf@gmail.com>
2021-01-29 12:47:05 +0530

    Update default-configuration.js

--- src/widget/default-configuration.js
+++ src/widget/default-configuration.js
@@ -3,13 +3,13 @@ export const defaultConfiguration = {
     titleClosed: '',
     titleOpen: '👋 You can ask anything!',
     closedStyle: 'chat', // button or chat
-    closedChatAvatarUrl: '', // only used if closedStyle is set to 'chat'
+    closedChatAvatarUrl: 'http://github.com/mddanishyusuf.png', // only used if closedStyle is set to 'chat'
     cookieExpiration: 1, // in days. Once opened, closed chat title will be shown as button (when closedStyle is set to 'chat')
     introMessage: 'Hello! How can we help?',
     autoResponse: 'Looking for the first available admin.',
-    autoNoResponse: 'It seems that My Team had a lot of work today. So, we are taking a small nap. Please tell us how we can contact you, and we will get back to you ASAP.',
+    autoNoResponse: 'It seems that My Team had a lot of work today. Please tell us how we can help you, and we will get back to you ASAP.',
     placeholderText: '🚀 Send a message...',
-    displayMessageTime: false,
+    displayMessageTime: true,
     mainColor: '#ffcd58',
     alwaysUseFloatingButton: true,
     desktopHeight: 450,
